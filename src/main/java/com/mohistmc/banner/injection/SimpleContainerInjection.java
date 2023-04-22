58f81809059eff6cd56a9a6616bb3103b9cb53d0
1798643961 <1798643961@qq.com>
2023-04-22 18:01:15 +0800

    to:sth injections

---
+++ src/main/java/com/mohistmc/banner/injection/SimpleContainerInjection.java
@@ -0,0 +1,16 @@
+package com.mohistmc.banner.injection;
+
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
+public interface SimpleContainerInjection {
+
+    default List<ItemStack> getContents() {
+        return null;
+    }
+
+    default void setMaxStackSize(int i) {
+
+    }
+}
