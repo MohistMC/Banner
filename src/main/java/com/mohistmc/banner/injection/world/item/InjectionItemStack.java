349a2019efc286875dadae566bc9859d21320e7b
1798643961 <1798643961@qq.com>
2023-04-25 18:21:51 +0800

    to:item stack injections

---
+++ src/main/java/com/mohistmc/banner/injection/world/item/InjectionItemStack.java
@@ -0,0 +1,32 @@
+package com.mohistmc.banner.injection.world.item;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import org.jetbrains.annotations.Nullable;
+
+public interface InjectionItemStack {
+
+    default void convertStack(int version) {
+    }
+
+    default void load(CompoundTag nbttagcompound) {
+    }
+
+    default InteractionResult useOn(UseOnContext itemactioncontext, InteractionHand enumhand) { // CraftBukkit - add hand
+        return null;
+    }
+
+    default CompoundTag getTagClone() {
+        return null;
+    }
+
+    default void setTagClone(@Nullable CompoundTag nbtttagcompound) {
+    }
+
+    @Deprecated
+    default void setItem(Item item) {
+    }
+}
