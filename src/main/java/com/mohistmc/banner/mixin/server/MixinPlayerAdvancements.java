2de94d33882c031967f19acd3c549b73cdaea65f
1798643961 <1798643961@qq.com>
2023-04-27 19:49:39 +0800

    feat:Implements PlayerAdvancementDoneEvent

---
+++ src/main/java/com/mohistmc/banner/mixin/server/MixinPlayerAdvancements.java
@@ -0,0 +1,23 @@
+package com.mohistmc.banner.mixin.server;
+
+import net.minecraft.advancements.Advancement;
+import net.minecraft.server.PlayerAdvancements;
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.Bukkit;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerAdvancements.class)
+public abstract class MixinPlayerAdvancements {
+
+    @Shadow private ServerPlayer player;
+
+    @Inject(method = "award",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/Advancement;getRewards()Lnet/minecraft/advancements/AdvancementRewards;"))
+    public void arclight$callEvent(Advancement advancement, String criterionKey, CallbackInfoReturnable<Boolean> cir) {
+        Bukkit.getPluginManager().callEvent(new org.bukkit.event.player.PlayerAdvancementDoneEvent(this.player.getBukkitEntity(), advancement.bridge$bukkit()));
+    }
+}
