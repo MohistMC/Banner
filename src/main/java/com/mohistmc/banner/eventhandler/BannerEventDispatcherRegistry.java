6326e89478654e33953ffba8268e710c6ae74cfe
1798643961 <1798643961@qq.com>
2023-05-07 12:02:28 +0800

    Try fix bugs by migrate methods

--- src/main/java/com/mohistmc/banner/eventhandler/BannerEventDispatcherRegistry.java
+++ src/main/java/com/mohistmc/banner/eventhandler/BannerEventDispatcherRegistry.java
@@ -8,7 +8,6 @@ public class BannerEventDispatcherRegistry {
     public static void registerEventDispatchers() {
         BannerServer.LOGGER.info("Registering Banner Event Dispatchers...");
         ServerEventDispatcher.dispatchServer();
-        LevelEventDispatcher.dispatchLevel();
         PlayerEventDispatcher.dispatcherPlayer();
         CommandsEventDispatcher.onCommandDispatch();
         EntityEventDispatcher.dispatchEntity();
