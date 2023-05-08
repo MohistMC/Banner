12348593dcd49b666b75ff9e33707988910b4106
1798643961 <1798643961@qq.com>
2023-05-08 15:20:21 +0800

    refactor: use Atomic to handle Block Place

--- src/main/java/com/mohistmc/banner/mixin/world/item/MixinBlockItem.java
+++ src/main/java/com/mohistmc/banner/mixin/world/item/MixinBlockItem.java
@@ -7,9 +7,11 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PlaceOnWaterBlockItem;
 import net.minecraft.world.item.SolidBucketItem;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -30,6 +32,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 @Mixin(BlockItem.class)
 public abstract class MixinBlockItem {
 
@@ -38,37 +42,31 @@ public abstract class MixinBlockItem {
     @Shadow private static <T extends Comparable<T>> BlockState updateState(BlockState p_219988_0_, Property<T> p_219988_1_, String p_219988_2_) { return null; }
     // @formatter:on
 
-    private transient org.bukkit.block.BlockState banner$state;
+    private AtomicReference<org.bukkit.block.BlockState> banner$state = new AtomicReference<>();
 
     @Inject(method = "place", locals = LocalCapture.CAPTURE_FAILHARD,
             at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/world/item/BlockItem;getPlacementState(Lnet/minecraft/world/item/context/BlockPlaceContext;)Lnet/minecraft/world/level/block/state/BlockState;"))
     private void banner$prePlaceLilypad(BlockPlaceContext context, CallbackInfoReturnable<InteractionResult> cir, BlockPlaceContext context1) {
         if ((Object) this instanceof PlaceOnWaterBlockItem || (Object) this instanceof SolidBucketItem) {
-            this.banner$state = CraftBlockStates.getBlockState(context1.getLevel(), context1.getClickedPos());
+            this.banner$state.set(CraftBlockStates.getBlockState(context1.getLevel(), context1.getClickedPos()));
         }
     }
 
     @Inject(method = "place", locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true,
-            at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/world/level/block/Block;setPlacedBy(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;)V"))
-    private void banner$postPlaceLilypad(BlockPlaceContext context, CallbackInfoReturnable<InteractionResult> cir, BlockPlaceContext context1) {
-        org.bukkit.block.BlockState state = banner$state;
-        banner$state = null;
-        BlockPos pos = context1.getClickedPos();
-        if (state != null && DistValidate.isValid(context)) {
-            org.bukkit.event.block.BlockPlaceEvent placeEvent = CraftEventFactory.callBlockPlaceEvent((ServerLevel) context1.getLevel(), context1.getPlayer(), context1.getHand(), state, pos.getX(), pos.getY(), pos.getZ());
+            at = @At(value = "INVOKE", shift = At.Shift.AFTER,
+                    target = "Lnet/minecraft/world/level/block/Block;setPlacedBy(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;)V"))
+    private void banner$postPlaceLilypad(BlockPlaceContext context, CallbackInfoReturnable<InteractionResult> cir, BlockPlaceContext blockPlaceContext, BlockState blockState, BlockPos blockPos, Level level, net.minecraft.world.entity.player.Player player, ItemStack itemStack, BlockState blockState2) {
+        // CraftBukkit start
+        if (banner$state.get() != null) {
+            org.bukkit.event.block.BlockPlaceEvent placeEvent = CraftEventFactory.callBlockPlaceEvent((ServerLevel) level, player, blockPlaceContext.getHand(), banner$state.get(), blockPos.getX(), blockPos.getY(), blockPos.getZ());
             if (placeEvent != null && (placeEvent.isCancelled() || !placeEvent.canBuild())) {
-                state.update(true, false);
-                if ((Object) this instanceof SolidBucketItem) {
-                     ((ServerPlayer) context1.getPlayer()).getBukkitEntity().updateInventory();
-                }
-                cir.setReturnValue(InteractionResult.FAIL);
+                banner$state.get().update(true, false);
             }
+            if ((Object) this instanceof SolidBucketItem) {
+                ((ServerPlayer) player).getBukkitEntity().updateInventory(); // SPIGOT-4541
+            }
+            cir.setReturnValue(InteractionResult.FAIL);
         }
-    }
-
-    @Inject(method = "place", at = @At("RETURN"))
-    private void banner$cleanup(BlockPlaceContext context, CallbackInfoReturnable<InteractionResult> cir) {
-        this.banner$state = null;
     }
 
     private static BlockState getBlockState(BlockState blockState, CompoundTag nbt) {
