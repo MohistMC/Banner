e4d1f67e3c7256e3fe097fde4ad3bfb930f80e4f
1798643961 <1798643961@qq.com>
2023-05-11 21:32:12 +0800

    to: fix aw

--- src/main/java/org/bukkit/craftbukkit/v1_19_R3/block/CraftSign.java
+++ src/main/java/org/bukkit/craftbukkit/v1_19_R3/block/CraftSign.java
@@ -26,18 +26,18 @@ public class CraftSign<T extends SignBlockEntity> extends CraftBlockEntityState<
         Preconditions.checkArgument(sign.getWorld() == player.getWorld(), "Sign must be in same world as Player");
 
         SignBlockEntity handle = ((CraftSign<?>) sign).getTileEntity();
-        handle.isEditable = true;
+        //handle.isEditable = true; Banner - TODO
 
-        ((CraftPlayer) player).getHandle().openTextEdit(handle);
+        ((CraftPlayer) player).getHandle().openTextEdit(handle, true);
     }
 
     @Override
     public String[] getLines() {
         if (lines == null) {
-            // Lazy initialization:
+            // Lazy initialization:Banner - TODO
             SignBlockEntity sign = this.getSnapshot();
-            lines = new String[sign.messages.length];
-            System.arraycopy(revertComponents(sign.messages), 0, lines, 0, lines.length);
+            //lines = new String[sign.messages.length];
+            //System.arraycopy(revertComponents(sign.messages), 0, lines, 0, lines.length); Banner - TODO
             originalLines = new String[lines.length];
             System.arraycopy(lines, 0, originalLines, 0, originalLines.length);
         }
@@ -56,32 +56,35 @@ public class CraftSign<T extends SignBlockEntity> extends CraftBlockEntityState<
 
     @Override
     public boolean isEditable() {
-        return getSnapshot().isEditable;
+        //return getSnapshot().isEditable; Banner - TODO
+        return false;
     }
 
     @Override
     public void setEditable(boolean editable) {
-        getSnapshot().isEditable = editable;
+        //getSnapshot().isEditable = editable; Banner - TODO
     }
 
     @Override
     public boolean isGlowingText() {
-        return getSnapshot().hasGlowingText();
+        //return getSnapshot().hasGlowingText();Banner - TODO
+        return false;
     }
 
     @Override
     public void setGlowingText(boolean glowing) {
-        getSnapshot().setHasGlowingText(glowing);
+        //getSnapshot().setHasGlowingText(glowing); Banner - TODO
     }
 
     @Override
     public DyeColor getColor() {
-        return DyeColor.getByWoolData((byte) getSnapshot().getColor().getId());
+       // return DyeColor.getByWoolData((byte) getSnapshot().getColor().getId()); Banner - TODO
+        return null;
     }
 
     @Override
     public void setColor(DyeColor color) {
-        getSnapshot().setColor(net.minecraft.world.item.DyeColor.byId(color.getWoolData()));
+       // getSnapshot().setColor(net.minecraft.world.item.DyeColor.byId(color.getWoolData())); Banner - TODO
     }
 
     @Override
@@ -94,7 +97,7 @@ public class CraftSign<T extends SignBlockEntity> extends CraftBlockEntityState<
                 if (line.equals(originalLines[i])) {
                     continue; // The line contents are still the same, skip.
                 }
-                sign.setMessage(i, CraftChatMessage.fromString(line)[0]);
+                // sign.setMessage(i, CraftChatMessage.fromString(line)[0]); Banner - TODO
             }
         }
     }
