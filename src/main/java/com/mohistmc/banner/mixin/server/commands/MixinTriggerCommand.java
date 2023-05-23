8f72fe8784276940a9c91b2f94fdc8fc27c1b59b
1798643961 <1798643961@qq.com>
2023-05-23 19:45:11 +0800

    fix missing patches

---
+++ src/main/java/com/mohistmc/banner/mixin/server/commands/MixinTriggerCommand.java
@@ -0,0 +1,18 @@
+package com.mohistmc.banner.mixin.server.commands;
+
+import net.minecraft.server.commands.TriggerCommand;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.scores.Scoreboard;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TriggerCommand.class)
+public class MixinTriggerCommand {
+
+    @Redirect(method = "getScore", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/server/level/ServerPlayer;getScoreboard()Lnet/minecraft/world/scores/Scoreboard;"))
+    private static Scoreboard banner$resetScore(ServerPlayer instance) {
+        return instance.getServer().getScoreboard();// CraftBukkit - SPIGOT-6917: use main scoreboard
+    }
+}
