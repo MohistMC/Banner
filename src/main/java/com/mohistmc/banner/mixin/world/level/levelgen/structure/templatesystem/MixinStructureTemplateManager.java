0f5ee1e25f7a4275514af7624e1bba6f08aa20b4
1798643961 <1798643961@qq.com>
2023-05-25 18:02:52 +0800

    clear code

--- src/main/java/com/mohistmc/banner/mixin/world/level/levelgen/structure/templatesystem/MixinStructureTemplateManager.java
+++ src/main/java/com/mohistmc/banner/mixin/world/level/levelgen/structure/templatesystem/MixinStructureTemplateManager.java
@@ -4,8 +4,6 @@ import com.mohistmc.banner.injection.world.level.levelgen.structure.templatesyst
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -16,12 +14,11 @@ public abstract class MixinStructureTemplateManager implements InjectionStructur
 
     @Shadow public abstract Optional<StructureTemplate> loadFromResource(ResourceLocation id);
 
-    @Shadow @Final private static Logger LOGGER;
-
     // Banner start Fix modernfix mod
     @Override
     public Optional<StructureTemplate> loadFromResource0(ResourceLocation id) {
         return loadFromResource(id);
     }
     // Banner end
+
 }
