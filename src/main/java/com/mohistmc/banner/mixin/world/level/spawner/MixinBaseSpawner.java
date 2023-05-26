fba6ae2ad253974b67c831c353e15fc1db543d86
1798643961 <1798643961@qq.com>
2023-05-26 14:37:11 +0800

    Implements SpawnerSpawnEvent

--- src/main/java/com/mohistmc/banner/mixin/world/level/spawner/MixinBaseSpawner.java
+++ src/main/java/com/mohistmc/banner/mixin/world/level/spawner/MixinBaseSpawner.java
@@ -1,16 +1,24 @@
 package com.mohistmc.banner.mixin.world.level.spawner;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.util.random.SimpleWeightedRandomList;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BaseSpawner;
 import net.minecraft.world.level.SpawnData;
+import org.bukkit.craftbukkit.v1_19_R3.event.CraftEventFactory;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Optional;
 
 @Mixin(BaseSpawner.class)
 public abstract class MixinBaseSpawner {
@@ -25,8 +33,25 @@ public abstract class MixinBaseSpawner {
 
     @Inject(method = "serverTick",
             at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/server/level/ServerLevel;tryAddFreshEntityWithPassengers(Lnet/minecraft/world/entity/Entity;)Z"))
-    private void banner$pushReason(ServerLevel serverLevel, BlockPos pos, CallbackInfo ci) {
+            target = "Lnet/minecraft/server/level/ServerLevel;tryAddFreshEntityWithPassengers(Lnet/minecraft/world/entity/Entity;)Z"),
+            locals = LocalCapture.CAPTURE_FAILHARD)
+    private void banner$pushReason(ServerLevel serverLevel, BlockPos pos, CallbackInfo ci, boolean bl,
+                                   RandomSource randomSource, SpawnData spawnData, int i,
+                                   CompoundTag compoundTag, Optional optional, ListTag listTag,
+                                   int j, double d, double e, double f, BlockPos blockPos,
+                                   Entity entity) {
+        // Spigot Start
+        if (CraftEventFactory.callSpawnerSpawnEvent(entity, blockPos).isCancelled()) {
+            Entity vehicle = entity.getVehicle();
+            if (vehicle != null) {
+                vehicle.discard();
+            }
+            for (Entity passenger : entity.getIndirectPassengers()) {
+                passenger.discard();
+            }
+            return;
+        }
+        // Spigot End
         serverLevel.pushAddEntityReason(CreatureSpawnEvent.SpawnReason.SPAWNER);
     }
 }
