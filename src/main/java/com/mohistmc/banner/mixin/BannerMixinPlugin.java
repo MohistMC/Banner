f06933a0365940c73733f5d9c782e803e9febabe
1798643961 <1798643961@qq.com>
2023-06-10 19:25:39 +0800

    Fix compat

--- src/main/java/com/mohistmc/banner/mixin/BannerMixinPlugin.java
+++ src/main/java/com/mohistmc/banner/mixin/BannerMixinPlugin.java
@@ -133,6 +133,9 @@ public class BannerMixinPlugin implements IMixinConfigPlugin {
                 || mixinClassName.equals("com.mohistmc.banner.mixin.server.players.MixinPlayerList$LoadRecursive")) {
             return !FabricLoader.getInstance().isModLoaded("vmp");
         }
+        if (mixinClassName.equals("com.mohistmc.banner.mixin.world.level.spawner.MixinNaturalSpawner")) {
+            return !FabricLoader.getInstance().isModLoaded("carpet-tis-addition");
+        }
         return true;
     }
 
