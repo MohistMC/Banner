f59c13a4f6217f5f51b0f78d89b59ca7b7536ca2
1798643961 <1798643961@qq.com>
2023-06-17 19:49:29 +0800

    implements some paper event

--- src/main/java/com/mohistmc/banner/mixin/world/entity/moster/MixinEnderMan.java
+++ src/main/java/com/mohistmc/banner/mixin/world/entity/moster/MixinEnderMan.java
@@ -8,12 +8,19 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.Vec3;
 import org.bukkit.event.entity.EntityTargetEvent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
 
@@ -56,9 +63,32 @@ public abstract class MixinEnderMan extends Monster {
         return true;
     }
 
+    @Inject(method = "isLookingAtMe", at = @At("HEAD"), cancellable = true)
+    private void banner$lookingCheck(Player player, CallbackInfoReturnable<Boolean> cir) {
+        boolean shouldAttack = isLookingAtMe_check(player);
+        com.destroystokyo.paper.event.entity.EndermanAttackPlayerEvent event = new com.destroystokyo.paper.event.entity.EndermanAttackPlayerEvent((org.bukkit.entity.Enderman) getBukkitEntity(), (org.bukkit.entity.Player) player.getBukkitEntity());
+        event.setCancelled(!shouldAttack);
+        cir.setReturnValue(event.callEvent());
+        cir.cancel();
+    }
+
+    private boolean isLookingAtMe_check(Player player) {
+        ItemStack itemStack = (ItemStack) player.getInventory().armor.get(3);
+        if (itemStack.is(Blocks.CARVED_PUMPKIN.asItem())) {
+            return false;
+        } else {
+            Vec3 vec3 = player.getViewVector(1.0F).normalize();
+            Vec3 vec32 = new Vec3(this.getX() - player.getX(), this.getEyeY() - player.getEyeY(), this.getZ() - player.getZ());
+            double d = vec32.length();
+            vec32 = vec32.normalize();
+            double e = vec3.dot(vec32);
+            return e > 1.0 - 0.025 / d ? player.hasLineOfSight(this) : false;
+        }
+    }
+
     /**
      * @author wdog5
-     * @reason
+     * @reason bukkit
      */
     @Overwrite
     public void setTarget(@Nullable LivingEntity entity) {
