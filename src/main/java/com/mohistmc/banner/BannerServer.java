37fd34b0f1b1f3caf3704ca257bbb6a69230d56a
1798643961 <1798643961@qq.com>
2023-06-23 00:28:47 +0800

    Random fix

--- src/main/java/com/mohistmc/banner/BannerServer.java
+++ src/main/java/com/mohistmc/banner/BannerServer.java
@@ -1,13 +1,8 @@
 package com.mohistmc.banner;
 
 import com.mohistmc.banner.eventhandler.BannerEventDispatcherRegistry;
-import com.mohistmc.banner.stackdeobf.mappings.CachedMappings;
-import com.mohistmc.banner.stackdeobf.mappings.providers.MojangMappingProvider;
-import com.mohistmc.banner.stackdeobf.util.CompatUtil;
-import com.mohistmc.banner.stackdeobf.util.RemappingRewritePolicy;
 import io.izzel.arclight.mixin.injector.EjectorInfo;
 import net.fabricmc.api.DedicatedServerModInitializer;
-import org.apache.logging.log4j.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.injection.struct.InjectionInfo;
