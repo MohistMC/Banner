a9541b9a166f9eabacdb03f6d2a041010baf1939
1798643961 <1798643961@qq.com>
2023-06-27 10:55:27 +0800

    Fix Unhandled statistic

--- src/main/java/com/mohistmc/banner/fabric/FabricInjectBukkit.java
+++ src/main/java/com/mohistmc/banner/fabric/FabricInjectBukkit.java
@@ -13,6 +13,7 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.stats.StatType;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.npc.VillagerProfession;
@@ -27,6 +28,7 @@ import org.bukkit.Fluid;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
+import org.bukkit.Statistic;
 import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_19_R3.enchantments.CraftEnchantment;
 import org.bukkit.craftbukkit.v1_19_R3.potion.CraftPotionEffectType;
@@ -52,6 +54,7 @@ public class FabricInjectBukkit {
                     .build());
     public static Map<Villager.Profession, ResourceLocation> profession = new HashMap<>();
     public static Map<org.bukkit.attribute.Attribute, ResourceLocation> attributemap = new HashMap<>();
+    public static Map<StatType<?>, Statistic> statisticMap = new HashMap<>();
 
     public static void init() {
         addEnumMaterialInItems();
@@ -63,6 +66,7 @@ public class FabricInjectBukkit {
         addEnumEntity();
         addEnumVillagerProfession();
         addEnumParticle();
+        addStatistic();
     }
 
     public static void addEnumEnvironment() {
@@ -219,6 +223,20 @@ public class FabricInjectBukkit {
             }
         }
     }
+
+    public static void addStatistic() {
+        var registry = BuiltInRegistries.STAT_TYPE;
+        for (StatType<?> statType : registry) {
+            ResourceLocation resourceLocation = registry.getKey(statType);
+            String name = normalizeName(resourceLocation.getPath());
+            if (!isMINECRAFT(resourceLocation)) {
+                Statistic statistic = MohistDynamEnum.addEnum0(Statistic.class, name, new Class[0]);
+                statisticMap.put(statType, statistic);
+                BannerServer.LOGGER.debug("Registered fabric StatType as Statistic(Bukkit) {}", statistic.name());
+            }
+        }
+    }
+
 
     public static String normalizeName(String name) {
         return name.toUpperCase(java.util.Locale.ENGLISH).replaceAll("(:|\\s)", "_").replaceAll("\\W", "");
--- src/main/java/org/bukkit/craftbukkit/v1_19_R3/event/CraftEventFactory.java
+++ src/main/java/org/bukkit/craftbukkit/v1_19_R3/event/CraftEventFactory.java
@@ -3,6 +3,7 @@ package org.bukkit.craftbukkit.v1_19_R3.event;
 import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.collect.Lists;
+import com.mohistmc.banner.fabric.FabricInjectBukkit;
 import com.mojang.datafixers.util.Either;
 import java.net.InetAddress;
 import java.util.*;
@@ -1443,6 +1444,9 @@ public class CraftEventFactory {
         Event event;
         if (true) {
             org.bukkit.Statistic stat = CraftStatistic.getBukkitStatistic(statistic);
+            if (stat == null) {
+                stat = FabricInjectBukkit.statisticMap.get(statistic.getType());
+            }
             if (stat == null) {
                 System.err.println("Unhandled statistic: " + statistic);
                 return null;
