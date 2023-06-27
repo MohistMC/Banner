841deb65dc687f6a02b6c5e0637ccf445fa1f273
1798643961 <1798643961@qq.com>
2023-06-27 11:06:29 +0800

    Fix world name

--- src/main/java/org/bukkit/craftbukkit/v1_20_R1/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/v1_20_R1/CraftServer.java
@@ -1089,6 +1089,10 @@ public final class CraftServer implements Server {
 
         ServerLevel internal = new ServerLevel(console, console.executor, worldSession, worlddata, worldKey, worlddimension, getServer().progressListenerFactory.create(11),
                 worlddata.isDebugWorld(), j, creator.environment() == Environment.NORMAL ? list : ImmutableList.of(), true, console.overworld().getRandomSequences());
+        if (name.contains("/")) {
+            String[] strings = name.split("/");
+            name = strings[strings.length - 1];
+        }
         internal.banner$setGenerator(generator);
         internal.banner$setBiomeProvider(biomeProvider);
         if (!(worlds.containsKey(name.toLowerCase(java.util.Locale.ENGLISH)))) {
