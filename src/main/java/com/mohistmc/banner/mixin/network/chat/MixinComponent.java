b0ec32f9f80b11af36bfe1a88e294372a9737ae7
1798643961 <1798643961@qq.com>
2023-07-07 15:35:41 +0800

    Fix Component

--- src/main/java/com/mohistmc/banner/mixin/network/chat/MixinComponent.java
+++ src/main/java/com/mohistmc/banner/mixin/network/chat/MixinComponent.java
@@ -3,6 +3,8 @@ package com.mohistmc.banner.mixin.network.chat;
 import com.google.common.collect.Streams;
 import com.mohistmc.banner.injection.network.chat.InjectionComponent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -38,5 +40,9 @@ public interface MixinComponent extends Iterable<Component>, InjectionComponent 
     @Override
     default Stream<Component> bridge$stream() {
         return stream();
+    }
+
+    private static MutableComponent a(String key, Object... args) {
+        return MutableComponent.create(new TranslatableContents(key, (String)null, args));
     }
 }
