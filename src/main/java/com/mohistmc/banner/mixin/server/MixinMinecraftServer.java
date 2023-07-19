ef99509f0f3d3eb03809d7e3439098e0cc3d2264
1798643961 <1798643961@qq.com>
2023-07-19 15:51:49 +0800

    Fix no set generator/biome/chunk

--- src/main/java/com/mohistmc/banner/mixin/server/MixinMinecraftServer.java
+++ src/main/java/com/mohistmc/banner/mixin/server/MixinMinecraftServer.java
@@ -51,6 +51,7 @@ import net.minecraft.world.level.storage.LevelStorageSource;
 import net.minecraft.world.level.storage.ServerLevelData;
 import net.minecraft.world.level.storage.WorldData;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.RemoteConsoleCommandSender;
 import org.bukkit.craftbukkit.Main;
@@ -321,6 +322,10 @@ public abstract class MixinMinecraftServer extends ReentrantBlockableEventLoop<T
                                   ServerLevelData serverLevelData, boolean bl, Registry registry,
                                   WorldOptions worldOptions, long l, long m, List list, LevelStem levelStem,
                                   ServerLevel serverLevel) {
+        String levelName = getWorldData().getLevelName();
+        serverLevel.banner$setGenerator(this.server.getGenerator(levelName));
+        serverLevel.banner$setEnvironment(World.Environment.NORMAL);
+        serverLevel.banner$setBiomeProvider(this.server.getBiomeProvider(levelName));
         initWorld(serverLevel, serverLevelData, worldData, worldOptions);
     }
 
