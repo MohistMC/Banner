3a60f6e7a63bfefc9929dab639560475a2587724
1798643961 <1798643961@qq.com>
2023-07-22 21:47:45 +0800

    Remove Bukkit worlds by Fabric

--- src/main/java/com/mohistmc/banner/eventhandler/dispatcher/LevelEventDispatcher.java
+++ src/main/java/com/mohistmc/banner/eventhandler/dispatcher/LevelEventDispatcher.java
@@ -2,6 +2,9 @@ package com.mohistmc.banner.eventhandler.dispatcher;
 
 import com.mohistmc.banner.fabric.FabricHookBukkitEvent;
 import com.mohistmc.banner.fabric.FabricInjectBukkit;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_20_R1.CraftServer;
 import org.bukkit.event.world.WorldInitEvent;
 
 public class LevelEventDispatcher {
@@ -11,6 +14,9 @@ public class LevelEventDispatcher {
             if (bukkitEvent instanceof WorldInitEvent) {
                 FabricInjectBukkit.addEnumEnvironment(); // Banner
             }
+        });
+        ServerWorldEvents.UNLOAD.register((server, world) -> {
+            ((CraftServer) Bukkit.getServer()).removeWorld(world);
         });
     }
 }
