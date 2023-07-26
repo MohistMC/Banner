0e616ec18e5627e66661bed4ed60a74e7e4ed3d6
1798643961 <1798643961@qq.com>
2023-07-26 13:27:52 +0800

    Fix ClipContext null(for slimefun)

--- src/main/java/com/mohistmc/banner/mixin/world/level/MixinClipContext.java
+++ src/main/java/com/mohistmc/banner/mixin/world/level/MixinClipContext.java
@@ -2,16 +2,13 @@ package com.mohistmc.banner.mixin.world.level;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.ClipContext;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClipContext.class)
 public class MixinClipContext {
@@ -19,10 +16,9 @@ public class MixinClipContext {
     @Mutable
     @Shadow @Final private CollisionContext collisionContext;
 
-    @Inject(method = "<init>", at = @At("RETURN"),
-            locals = LocalCapture.CAPTURE_FAILHARD)
-    private void banner$resetClipContext(Vec3 from, Vec3 _to, ClipContext.Block block, ClipContext.Fluid fluid,
-                                         Entity entity, CallbackInfo ci) {
-        this.collisionContext = (entity == null) ? CollisionContext.empty() : CollisionContext.of(entity); // CraftBukkit
+    @Redirect(method = "<init>", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/phys/shapes/CollisionContext;of(Lnet/minecraft/world/entity/Entity;)Lnet/minecraft/world/phys/shapes/CollisionContext;"))
+    private CollisionContext banner$resetClipContext(Entity entity) {
+        return (entity == null) ? CollisionContext.empty() : CollisionContext.of(entity); // CraftBukkit;
     }
 }
