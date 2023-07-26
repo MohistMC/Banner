a4caf9a4635f8c43ea7aaa2521bf05d5c1b1ce7f
Mgazul <mgazul@163.com>
2023-07-26 21:56:53 +0800

    Added usage tips

---
+++ src/main/java/com/mohistmc/banner/BannerGUI.java
@@ -0,0 +1,14 @@
+package com.mohistmc.banner;
+
+/**
+ * @author Mgazul by MohistMC
+ * @date 2023/7/26 21:51:45
+ */
+public class BannerGUI {
+
+    public static void main(String[] s) {
+        System.out.println("Please put Banner into mods folder and run Fabric server");
+        System.out.println("Please put Banner into mods folder and run Fabric server");
+        System.out.println("Please put Banner into mods folder and run Fabric server");
+    }
+}
