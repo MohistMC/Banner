dff720a30943a00787901fb34bb9c6827d609f7c
1798643961 <1798643961@qq.com>
2023-07-30 14:24:32 +0800

    Some fix of end portal

--- src/main/java/com/mohistmc/banner/mixin/world/level/block/MixinEndPortalBlock.java
+++ src/main/java/com/mohistmc/banner/mixin/world/level/block/MixinEndPortalBlock.java
@@ -1,10 +1,6 @@
 package com.mohistmc.banner.mixin.world.level.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.EndPortalBlock;
@@ -12,24 +8,19 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.event.entity.EntityPortalEnterEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EndPortalBlock.class)
 public class MixinEndPortalBlock {
 
-    @Redirect(method = "entityInside", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;getLevel(Lnet/minecraft/resources/ResourceKey;)Lnet/minecraft/server/level/ServerLevel;"))
-    public ServerLevel banner$enterPortal(MinecraftServer minecraftServer, ResourceKey<Level> dimension, BlockState state, Level worldIn, BlockPos pos, Entity entityIn) {
-        ServerLevel world = minecraftServer.getLevel(dimension);
-        EntityPortalEnterEvent event = new EntityPortalEnterEvent(entityIn.getBukkitEntity(), new org.bukkit.Location(world.getWorld(), pos.getX(), pos.getY(), pos.getZ()));
-                new Location(worldIn.getWorld(), pos.getX(), pos.getY(), pos.getZ());
+    @Inject(method = "entityInside", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;changeDimension(Lnet/minecraft/server/level/ServerLevel;)Lnet/minecraft/world/entity/Entity;"))
+    public void banner$enterPortal(BlockState state, Level level, BlockPos pos, Entity entity, CallbackInfo ci) {
+        EntityPortalEnterEvent event = new EntityPortalEnterEvent(entity.getBukkitEntity(),
+                new org.bukkit.Location(level.getWorld(), pos.getX(), pos.getY(), pos.getZ()));
+                new Location(level.getWorld(), pos.getX(), pos.getY(), pos.getZ());
         Bukkit.getPluginManager().callEvent(event);
-        if (entityIn instanceof ServerPlayer) {
-            ((ServerPlayer) entityIn).changeDimension(world, PlayerTeleportEvent.TeleportCause.END_PORTAL);
-            return null;
-        }
-        return world;
     }
 }
--- src/main/java/com/mohistmc/banner/mixin/world/level/block/entity/MixinTheEndGatewayBlockEntity.java
+++ src/main/java/com/mohistmc/banner/mixin/world/level/block/entity/MixinTheEndGatewayBlockEntity.java
@@ -12,6 +12,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_20_R1.block.CraftEndGateway;
 import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_20_R1.util.CraftLocation;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -36,7 +37,7 @@ public abstract class MixinTheEndGatewayBlockEntity extends TheEndPortalBlockEnt
     private static void banner$portal(Level level, BlockPos pos, BlockState state, Entity entityIn, TheEndGatewayBlockEntity blockEntity, CallbackInfo ci, BlockPos dest, Entity entity3) {
         if (entityIn instanceof ServerPlayer) {
             CraftPlayer player = ((ServerPlayer) entityIn).getBukkitEntity();
-            Location location = new Location(level.getWorld(), dest.getX() + 0.5D, dest.getY() + 0.5D, dest.getZ() + 0.5D);
+            Location location = CraftLocation.toBukkit(dest, level.getWorld()).add(0.5D, 0.5D, 0.5D);
             location.setPitch(player.getLocation().getPitch());
             location.setYaw(player.getLocation().getYaw());
 
