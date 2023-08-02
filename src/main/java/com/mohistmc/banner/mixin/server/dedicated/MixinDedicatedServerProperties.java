6206380b6f4a556afa89e669f80791130bea180f
1798643961 <1798643961@qq.com>
2023-08-02 21:47:53 +0800

    Don't check max tick time

---
+++ src/main/java/com/mohistmc/banner/mixin/server/dedicated/MixinDedicatedServerProperties.java
@@ -0,0 +1,25 @@
+package com.mohistmc.banner.mixin.server.dedicated;
+
+import net.minecraft.server.dedicated.DedicatedServerProperties;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Properties;
+
+@Mixin(DedicatedServerProperties.class)
+public class MixinDedicatedServerProperties {
+
+    @Mutable
+    @Shadow @Final
+    public long maxTickTime;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void banner$resetMaxTickTime(Properties properties, CallbackInfo ci) {
+        this.maxTickTime = -1;
+    }
+}
