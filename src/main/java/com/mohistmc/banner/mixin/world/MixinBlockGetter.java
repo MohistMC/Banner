0bd3fe7016d4fbf3b2b22d1aeb26d4ec8ec0e375
1798643961 <1798643961@qq.com>
2023-08-09 11:57:28 +0800

    Fix perm of command source

--- src/main/java/com/mohistmc/banner/mixin/world/MixinBlockGetter.java
+++ src/main/java/com/mohistmc/banner/mixin/world/MixinBlockGetter.java
@@ -23,18 +23,22 @@ public interface MixinBlockGetter extends InjectionBlockGetter {
     @Shadow @Nullable BlockHitResult clipWithInteractionOverride(Vec3 startVec, Vec3 endVec, BlockPos pos, VoxelShape shape, BlockState state);
 
     // CraftBukkit start - moved block handling into separate method for use by Block#rayTrace
+    @Override
     default BlockHitResult clip(ClipContext raytrace1, BlockPos blockposition) {
         BlockState iblockdata = this.getBlockState(blockposition);
         FluidState fluid = this.getFluidState(blockposition);
         Vec3 vec3d = raytrace1.getFrom();
         Vec3 vec3d1 = raytrace1.getTo();
-        VoxelShape voxelshape = raytrace1.getBlockShape(iblockdata, ((BlockGetter) (Object) this), blockposition);
-        BlockHitResult movingobjectpositionblock = this.clipWithInteractionOverride(vec3d, vec3d1, blockposition, voxelshape, iblockdata);
-        VoxelShape voxelshape1 = raytrace1.getFluidShape(fluid, ((BlockGetter) (Object) this), blockposition);
+        VoxelShape voxelshape = raytrace1.getBlockShape(iblockdata, (BlockGetter) (Object) this, blockposition);
+        BlockHitResult movingobjectpositionblock = this
+                .clipWithInteractionOverride(vec3d, vec3d1, blockposition, voxelshape,
+                        iblockdata);
+        VoxelShape voxelshape1 = raytrace1.getFluidShape(fluid, (BlockGetter) this, blockposition);
         BlockHitResult movingobjectpositionblock1 = voxelshape1.clip(vec3d, vec3d1, blockposition);
-        double d0 = movingobjectpositionblock == null ? Double.MAX_VALUE : raytrace1.getFrom().distanceToSqr(movingobjectpositionblock.getLocation());
-        double d1 = movingobjectpositionblock1 == null ? Double.MAX_VALUE : raytrace1.getFrom().distanceToSqr(movingobjectpositionblock1.getLocation());
-
+        double d0 = movingobjectpositionblock == null ? Double.MAX_VALUE :
+                raytrace1.getFrom().distanceToSqr(movingobjectpositionblock.getLocation());
+        double d1 = movingobjectpositionblock1 == null ? Double.MAX_VALUE :
+                raytrace1.getFrom().distanceToSqr(movingobjectpositionblock1.getLocation());
         return d0 <= d1 ? movingobjectpositionblock : movingobjectpositionblock1;
     }
     // CraftBukkit end
