012356b29988d95e48ed01dfdfa25fe40d9edaf4
1798643961 <1798643961@qq.com>
2023-08-19 11:36:43 +0800

    Handle if create load

---
+++ src/main/java/com/mohistmc/banner/mixin/world/level/MixinClipContext.java
@@ -0,0 +1,30 @@
+package com.mohistmc.banner.mixin.world.level;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ClipContext.class)
+public class MixinClipContext {
+
+    @Mutable
+    @Shadow @Final private CollisionContext collisionContext;
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/phys/shapes/CollisionContext;of(Lnet/minecraft/world/entity/Entity;)Lnet/minecraft/world/phys/shapes/CollisionContext;"))
+    private CollisionContext banner$resetClipContext(Entity entity) {
+        if (FabricLoader.getInstance().isModLoaded("create")
+                || FabricLoader.getInstance().isModLoaded("porting_lib")) {
+            return CollisionContext.of(entity);
+        }else{
+            return (entity == null) ? CollisionContext.empty() : CollisionContext.of(entity); // CraftBukkit;
+        }
+    }
+}
