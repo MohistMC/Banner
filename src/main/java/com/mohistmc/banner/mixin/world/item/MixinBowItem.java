1466a89e7a341f816933a7ce5fefcbee71683fed
wdog5 <1798643961@qq.com>
2024-02-17 20:35:48 +0800

    Fix some mod compat

--- src/main/java/com/mohistmc/banner/mixin/world/item/MixinBowItem.java
+++ src/main/java/com/mohistmc/banner/mixin/world/item/MixinBowItem.java
@@ -3,35 +3,27 @@ package com.mohistmc.banner.mixin.world.item;
 import com.mohistmc.banner.bukkit.DistValidate;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.stats.Stats;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ProjectileWeaponItem;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import org.bukkit.craftbukkit.v1_20_R1.event.CraftEventFactory;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(BowItem.class)
 public abstract class MixinBowItem extends ProjectileWeaponItem {
-
-
-    @Shadow
-    public static float getPowerForTime(int charge) {
-        return 0;
-    }
 
     public MixinBowItem(Properties properties) {
         super(properties);
@@ -39,93 +31,57 @@ public abstract class MixinBowItem extends ProjectileWeaponItem {
 
     @Unique
     private EntityShootBowEvent event;
+    @Unique
+    private Player banner$player;
 
-    /**
-     * @author wdog5
-     * @reason bukkit
-     */
-    @Overwrite
-    public void releaseUsing(ItemStack stack, Level level, LivingEntity livingEntity, int timeCharged) {
-        if (livingEntity instanceof Player player) {
-            boolean bl = player.getAbilities().instabuild || EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, stack) > 0;
-            ItemStack itemStack = player.getProjectile(stack);
-            if (!itemStack.isEmpty() || bl) {
-                if (itemStack.isEmpty()) {
-                    itemStack = new ItemStack(Items.ARROW);
-                }
-
-                int i = this.getUseDuration(stack) - timeCharged;
-                float f = getPowerForTime(i);
-                if (!((double)f < 0.1)) {
-                    boolean bl2 = bl && itemStack.is(Items.ARROW);
-                    if (!level.isClientSide) {
-                        ArrowItem arrowItem = (ArrowItem)(itemStack.getItem() instanceof ArrowItem ? itemStack.getItem() : Items.ARROW);
-                        AbstractArrow abstractArrow = arrowItem.createArrow(level, itemStack, player);
-                        abstractArrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, f * 3.0F, 1.0F);
-                        if (f == 1.0F) {
-                            abstractArrow.setCritArrow(true);
-                        }
-
-                        int j = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.POWER_ARROWS, stack);
-                        if (j > 0) {
-                            abstractArrow.setBaseDamage(abstractArrow.getBaseDamage() + (double)j * 0.5 + 0.5);
-                        }
-
-                        int k = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.PUNCH_ARROWS, stack);
-                        if (k > 0) {
-                            abstractArrow.setKnockback(k);
-                        }
-
-                        if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FLAMING_ARROWS, stack) > 0) {
-                            abstractArrow.setSecondsOnFire(100);
-                        }
-
-                        // CraftBukkit start
-                        event = CraftEventFactory.callEntityShootBowEvent(player, stack, itemStack,
-                                abstractArrow, player.getUsedItemHand(), f, !bl2);
-                        if (event.isCancelled()) {
-                            event.getProjectile().remove();
-                            return;
-                        }
-                        // CraftBukkit end
-
-                        stack.hurtAndBreak(1, player, (player2) -> {
-                            player2.broadcastBreakEvent(player.getUsedItemHand());
-                        });
-                        bl2 = !event.shouldConsumeItem(); // Banner
-                        if (bl2 || player.getAbilities().instabuild && (itemStack.is(Items.SPECTRAL_ARROW) || itemStack.is(Items.TIPPED_ARROW))) {
-                            abstractArrow.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
-                        }
-
-                        // CraftBukkit start
-                        if (event.getProjectile() == abstractArrow.getBukkitEntity()) {
-                            // Baner start - fix mixin
-                            level.addFreshEntity(abstractArrow);
-                            if (DistValidate.isValid(level)) {
-                                if (!((ServerLevel)level).canAddFreshEntity()) {
-                                    if (player instanceof ServerPlayer) {
-                                        ((ServerPlayer) player).getBukkitEntity().updateInventory();
-                                    }
-                                    return;
-                                }
-                            }
-                            // Banner end
-                        }
-                        // CraftBukkit end
-                    }
-
-                    level.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.PLAYERS, 1.0F, 1.0F / (level.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
-                    if (!bl2 && !player.getAbilities().instabuild) {
-                        itemStack.shrink(1);
-                        if (itemStack.isEmpty()) {
-                            player.getInventory().removeItem(itemStack);
-                        }
-                    }
-
-                    player.awardStat(Stats.ITEM_USED.get(this));
-                }
-            }
+    @Inject(method = "releaseUsing",
+            at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/item/ItemStack;hurtAndBreak(ILnet/minecraft/world/entity/LivingEntity;Ljava/util/function/Consumer;)V"),
+            locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void banner$shootBowEvent(ItemStack stack, Level level, LivingEntity livingEntity, int timeCharged,
+                                      CallbackInfo ci, Player player, boolean bl, ItemStack itemStack, int i,
+                                      float f, boolean bl2, ArrowItem arrowItem, AbstractArrow abstractArrow, int j, int k) {
+        banner$player = player;
+        // CraftBukkit start
+        event = CraftEventFactory.callEntityShootBowEvent(player, stack, itemStack,
+                abstractArrow, player.getUsedItemHand(), f, !bl2);
+        if (event.isCancelled()) {
+            event.getProjectile().remove();
+            ci.cancel();
         }
+        // CraftBukkit end
     }
 
+    @Inject(method = "releaseUsing",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/ItemStack;hurtAndBreak(ILnet/minecraft/world/entity/LivingEntity;Ljava/util/function/Consumer;)V",
+                    shift = At.Shift.AFTER),
+            locals = LocalCapture.CAPTURE_FAILHARD)
+    private void banner$shootBowEvent0(ItemStack stack, Level level, LivingEntity livingEntity, int timeCharged,
+                                      CallbackInfo ci, Player player, boolean bl, ItemStack itemStack, int i,
+                                      float f, boolean bl2, ArrowItem arrowItem, AbstractArrow abstractArrow, int j, int k) {
+        bl2 = !event.shouldConsumeItem(); // Banner
+    }
+
+    @Redirect(method = "releaseUsing",
+            at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"))
+    private boolean banner$checkAddEntity(Level level, Entity entity) {
+        // CraftBukkit start
+        if (event.getProjectile() == entity.getBukkitEntity()) {
+            // Baner start - fix mixin
+            level.addFreshEntity(entity);
+            if (DistValidate.isValid(level)) {
+                if (!((ServerLevel)level).canAddFreshEntity()) {
+                    if (banner$player instanceof ServerPlayer) {
+                        ((ServerPlayer) banner$player).getBukkitEntity().updateInventory();
+                    }
+                    return false;
+                }
+            }
+            // Banner end
+        }
+        // CraftBukkit end
+        return true;
+    }
 }
