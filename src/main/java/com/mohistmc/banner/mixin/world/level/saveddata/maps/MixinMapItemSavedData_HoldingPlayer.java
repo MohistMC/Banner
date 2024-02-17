0000d6750cde875078e6b1fac3052569d9b387fe
wdog5 <1798643961@qq.com>
2024-02-17 16:52:48 +0800

    Fix mixin compat

--- src/main/java/com/mohistmc/banner/mixin/world/level/saveddata/maps/MixinMapItemSavedData_HoldingPlayer.java
+++ src/main/java/com/mohistmc/banner/mixin/world/level/saveddata/maps/MixinMapItemSavedData_HoldingPlayer.java
@@ -1,68 +1,58 @@
 package com.mohistmc.banner.mixin.world.level.saveddata.maps;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
+
 import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientboundMapItemDataPacket;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.saveddata.maps.MapDecoration;
 import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
+import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_20_R1.map.RenderData;
 import org.bukkit.craftbukkit.v1_20_R1.util.CraftChatMessage;
-import org.bukkit.map.MapCursor;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MapItemSavedData.HoldingPlayer.class)
 public abstract class MixinMapItemSavedData_HoldingPlayer {
-    
-    // @formatter:off
-    @SuppressWarnings("target") @Shadow(aliases = {"field_132"}, remap = false) private MapItemSavedData outerThis;
-    @Shadow private boolean dirtyData;
-    @Shadow private int tick;
+
+    @Shadow @Final private MapItemSavedData field_132;
     @Shadow @Final public Player player;
-    @Shadow private boolean dirtyDecorations;
+    @Unique
+    private RenderData render = field_132.bridge$mapView().render((CraftPlayer) this.player.getBukkitEntity()); // CraftBukkit
+    @Unique
+    private byte[] banner$colors = field_132.colors;
+    @Unique
+    private java.util.Collection<MapDecoration> icons = new java.util.ArrayList<>();
 
-    @Shadow
-    protected abstract MapItemSavedData.MapPatch createPatch();
-    // @formatter:on
+    @Inject(method = "nextUpdatePacket", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData$HoldingPlayer;createPatch()Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData$MapPatch;"))
+    private void banner$checkColors(int mapId, CallbackInfoReturnable<Packet<?>> cir) {
+        field_132.colors = render.buffer;
+    }
 
-    /**
-     * @author wdog5
-     * @reason bukkit
-     */
-    @Overwrite
-    @Nullable
-    public Packet<?> nextUpdatePacket(int i) {
-        RenderData render =  outerThis.bridge$mapView().render(((ServerPlayer) this.player).getBukkitEntity()); // CraftBukkit        MapItemSavedData.MapPatch patch;
-        MapItemSavedData.MapPatch patch;
-        if (this.dirtyData) {
-            this.dirtyData = false;
-            var colors = outerThis.colors;
-            outerThis.colors = render.buffer;
-            patch = this.createPatch();
-            outerThis.colors = colors;
-        } else {
-            patch = null;
-        }
+    @Inject(method = "nextUpdatePacket", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData$HoldingPlayer;createPatch()Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData$MapPatch;",
+            shift = At.Shift.AFTER))
+    private void banner$setColors(int mapId, CallbackInfoReturnable<Packet<?>> cir) {
+        field_132.colors = banner$colors;
+    }
 
-        Collection<MapDecoration> icons;
-        if (this.tick++ % 5 == 0) {
-            this.dirtyDecorations = false;
-            icons = new ArrayList<>();
-            for (MapCursor cursor : render.cursors) {
-                if (cursor.isVisible()) {
-                    icons.add(new MapDecoration(MapDecoration.Type.byIcon(cursor.getRawType()),
-                            cursor.getX(), cursor.getY(), cursor.getDirection(), CraftChatMessage.fromStringOrNull(cursor.getCaption())));
-                }
+    @Redirect(method = "nextUpdatePacket", at = @At(value = "INVOKE", target = "Ljava/util/Map;values()Ljava/util/Collection;"))
+    private Collection<MapDecoration> banner$resetCollections(Map instance) {
+        // CraftBukkit start
+        for (org.bukkit.map.MapCursor cursor : render.cursors) {
+            if (cursor.isVisible()) {
+                icons.add(new MapDecoration(MapDecoration.Type.byIcon(cursor.getRawType()), cursor.getX(), cursor.getY(), cursor.getDirection(), CraftChatMessage.fromStringOrNull(cursor.getCaption())));
             }
-        } else {
-            icons = null;
         }
-        return icons == null && patch == null ? null : new ClientboundMapItemDataPacket(i, outerThis.scale, outerThis.locked, icons, patch);
+        return icons;
+        // CraftBukkit end
     }
 }
