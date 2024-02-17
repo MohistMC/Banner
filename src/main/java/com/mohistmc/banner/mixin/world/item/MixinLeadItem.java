a657b720c9174b9d035c09f6f6c209280c5b5f00
wdog5 <1798643961@qq.com>
2024-02-18 01:09:33 +0800

    Fixed #223

--- src/main/java/com/mohistmc/banner/mixin/world/item/MixinLeadItem.java
+++ src/main/java/com/mohistmc/banner/mixin/world/item/MixinLeadItem.java
@@ -1,5 +1,6 @@
 package com.mohistmc.banner.mixin.world.item;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 import net.minecraft.core.BlockPos;
@@ -17,61 +18,50 @@ import org.bukkit.craftbukkit.v1_20_R1.event.CraftEventFactory;
 import org.bukkit.event.hanging.HangingPlaceEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LeadItem.class)
 @SuppressWarnings("deprecation")
 public abstract class MixinLeadItem {
 
+    @Shadow
+    public static InteractionResult bindPlayerMobs(Player player, Level level, BlockPos pos) {
+        return null;
+    }
+
     @Unique
     private static AtomicReference<InteractionHand> banner$hand = new AtomicReference<>(InteractionHand.MAIN_HAND);
 
-    /**
-     * @author wdog5
-     * @reason bukkit event
-     */
-    @Overwrite
-    public static InteractionResult bindPlayerMobs(Player player, Level level, BlockPos pos) {
-        LeashFenceKnotEntity leashFenceKnotEntity = null;
-        boolean bl = false;
-        double d = 7.0;
-        int i = pos.getX();
-        int j = pos.getY();
-        int k = pos.getZ();
-        List<Mob> list = level.getEntitiesOfClass(Mob.class, new AABB((double)i - 7.0, (double)j - 7.0, (double)k - 7.0, (double)i + 7.0, (double)j + 7.0, (double)k + 7.0));
+    @Inject(method = "bindPlayerMobs",
+            at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/decoration/LeashFenceKnotEntity;playPlacementSound()V"),
+            locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private static void banner$bindPlayerMobs(Player player, Level level, BlockPos pos, CallbackInfoReturnable<InteractionResult> cir, LeashFenceKnotEntity leashFenceKnotEntity, boolean bl, double d, int i, int j, int k, List list, Iterator var11, Mob mob) {
+        // CraftBukkit start - fire HangingPlaceEvent
+        org.bukkit.inventory.EquipmentSlot hand = CraftEquipmentSlot.getHand(banner$hand.get());
+        HangingPlaceEvent event = new HangingPlaceEvent((org.bukkit.entity.Hanging) leashFenceKnotEntity.getBukkitEntity(), player != null ? (org.bukkit.entity.Player) player.getBukkitEntity() : null, level.getWorld().getBlockAt(i, j, k), org.bukkit.block.BlockFace.SELF, hand);
+        level.getCraftServer().getPluginManager().callEvent(event);
 
-        for (Mob mob : list) {
-            if (mob.getLeashHolder() == player) {
-                if (leashFenceKnotEntity == null) {
-                    leashFenceKnotEntity = LeashFenceKnotEntity.getOrCreateKnot(level, pos);
-                    // CraftBukkit start - fire HangingPlaceEvent
-                    org.bukkit.inventory.EquipmentSlot hand = CraftEquipmentSlot.getHand(banner$hand.get());
-                    HangingPlaceEvent event = new HangingPlaceEvent((org.bukkit.entity.Hanging) leashFenceKnotEntity.getBukkitEntity(), player != null ? (org.bukkit.entity.Player) player.getBukkitEntity() : null, level.getWorld().getBlockAt(i, j, k), org.bukkit.block.BlockFace.SELF, hand);
-                    level.getCraftServer().getPluginManager().callEvent(event);
-
-                    if (event.isCancelled()) {
-                        leashFenceKnotEntity.discard();
-                        return InteractionResult.PASS;
-                    }
-                    // CraftBukkit end
-                    leashFenceKnotEntity.playPlacementSound();
-                }
-
-                // CraftBukkit start
-                if (player != null && CraftEventFactory.callPlayerLeashEntityEvent(mob, leashFenceKnotEntity, player, banner$hand.get()).isCancelled()) {
-                    continue;
-                }
-
-                mob.setLeashedTo(leashFenceKnotEntity, true);
-                bl = true;
-            }
+        if (event.isCancelled()) {
+            leashFenceKnotEntity.discard();
+            cir.setReturnValue(InteractionResult.PASS);
         }
+        // CraftBukkit end
+    }
 
-        if (bl) {
-            level.gameEvent(GameEvent.BLOCK_ATTACH, pos, GameEvent.Context.of(player));
+    @Inject(method = "bindPlayerMobs", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/Mob;setLeashedTo(Lnet/minecraft/world/entity/Entity;Z)V"),
+            locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void banner$continueSet(Player player, Level level, BlockPos pos, CallbackInfoReturnable<InteractionResult> cir, LeashFenceKnotEntity leashFenceKnotEntity, boolean bl, double d, int i, int j, int k, List list, Iterator var11, Mob mob) {
+        // CraftBukkit start
+        if (player != null && CraftEventFactory.callPlayerLeashEntityEvent(mob, leashFenceKnotEntity, player, banner$hand.get()).isCancelled()) {
+            cir.cancel();
         }
-
-        return bl ? InteractionResult.SUCCESS : InteractionResult.PASS;
     }
 
     @Unique
