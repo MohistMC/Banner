bdecc43c3ec5e354dea7758f02057b1e4ba27d81
Mgazul <mgazul@163.com>
2024-02-26 11:34:28 +0700

    Fix async addEffect

--- src/main/java/com/mohistmc/banner/mixin/world/entity/MixinLivingEntity.java
+++ src/main/java/com/mohistmc/banner/mixin/world/entity/MixinLivingEntity.java
@@ -70,6 +70,7 @@ import org.bukkit.event.entity.EntityTeleportEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
+import org.spigotmc.AsyncCatcher;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -398,16 +399,18 @@ public abstract class MixinLivingEntity extends Entity implements InjectionLivin
             if (effectinstance != null) {
                 override = new MobEffectInstance(effectinstance).update(effectInstanceIn);
             }
-
-            EntityPotionEffectEvent event = CraftEventFactory.callEntityPotionEffectChangeEvent((LivingEntity) (Object) this, effectinstance, effectInstanceIn, cause, override);
-            if (event.isCancelled()) {
-                return false;
+            if (!AsyncCatcher.catchAsync()) {
+                EntityPotionEffectEvent event = CraftEventFactory.callEntityPotionEffectChangeEvent((LivingEntity) (Object) this, effectinstance, effectInstanceIn, cause, override);
+                if (event.isCancelled()) {
+                    return false;
+                }
+                override = event.isOverride();
             }
             if (effectinstance == null) {
                 this.activeEffects.put(effectInstanceIn.getEffect(), effectInstanceIn);
                 this.onEffectAdded(effectInstanceIn, entity);
                 return true;
-            } else if (event.isOverride()) {
+            } else if (override) {
                 effectinstance.update(effectInstanceIn);
                 this.onEffectUpdated(effectinstance, true, entity);
                 return true;
@@ -669,7 +672,7 @@ public abstract class MixinLivingEntity extends Entity implements InjectionLivin
 
     @Redirect(method = "die",
             at = @At(value = "INVOKE",
-            target = "Lorg/slf4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V",
+                    target = "Lorg/slf4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V",
                     remap = false))
     private void banner$logNamedDeaths(Logger instance, String s, Object o1, Object o2) {
         if (org.spigotmc.SpigotConfig.logNamedDeaths) LOGGER.info("Named entity {} died: {}", ((LivingEntity) (Object) this), this.getCombatTracker().getDeathMessage().getString()); // Spigot
@@ -977,7 +980,7 @@ public abstract class MixinLivingEntity extends Entity implements InjectionLivin
 
     @Override
     public void banner$setBukkitPickUpLoot(boolean bukkitPickUpLoot) {
-       this.bukkitPickUpLoot = bukkitPickUpLoot;
+        this.bukkitPickUpLoot = bukkitPickUpLoot;
     }
 
     @Override
--- src/main/java/org/spigotmc/AsyncCatcher.java
+++ src/main/java/org/spigotmc/AsyncCatcher.java
@@ -5,13 +5,23 @@ import com.mohistmc.banner.bukkit.BukkitExtraConstants;
 public class AsyncCatcher
 {
 
-    public static boolean enabled = false;
+    public static boolean enabled = true;
 
     public static void catchOp(String reason)
     {
-        if ( (AsyncCatcher.enabled || io.papermc.paper.util.TickThread.STRICT_THREAD_CHECKS) && Thread.currentThread() != BukkitExtraConstants.getServer().serverThread ) // Paper
+        if ( (enabled || io.papermc.paper.util.TickThread.STRICT_THREAD_CHECKS) && Thread.currentThread() != BukkitExtraConstants.getServer().serverThread ) // Paper
         {
             throw new IllegalStateException( "Asynchronous " + reason + "!" );
         }
     }
+
+    public static boolean catchAsync()
+    {
+        if ( enabled && Thread.currentThread() != BukkitExtraConstants.getServer().serverThread )
+        {
+            return true;
+        }
+        return false;
+    }
 }
+
