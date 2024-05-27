ab58541bae96d55aba30938b0adcfa8b47587522
Mgazul <mgazul@163.com>
2024-05-28 01:58:19 +0800

    Remove CN Source

--- src/main/java/com/mohistmc/banner/network/download/DownloadSource.java
+++ src/main/java/com/mohistmc/banner/network/download/DownloadSource.java
@@ -1,6 +1,5 @@
 package com.mohistmc.banner.network.download;
 
-import com.mohistmc.banner.BannerMCStart;
 import com.mohistmc.banner.config.BannerConfigUtil;
 import com.mohistmc.tools.ConnectionUtil;
 import lombok.AllArgsConstructor;
@@ -13,33 +12,20 @@ import lombok.ToString;
 public enum DownloadSource {
 
     MOHIST("https://maven.mohistmc.com/"),
-    CHINA("https://libraries.mohistmc.cn:25119/releases/"),
     GITHUB("https://mohistmc.github.io/maven/");
 
-    public static final DownloadSource defaultSource = isCN() ? CHINA : MOHIST;
     public final String url;
 
     public static DownloadSource get() {
         String ds = BannerConfigUtil.defaultSource();
-        DownloadSource urL;
         for (DownloadSource me : DownloadSource.values()) {
             if (me.name().equalsIgnoreCase(ds)) {
-                if (!ConnectionUtil.canAccess(me.url.replace("releases/", ""))) {
-                    if (ds.equals("CHINA")) {
-                        urL = MOHIST;
-                        if (!ConnectionUtil.canAccess(urL.url)) {
-                            return GITHUB;
-                        }
-                    }
+                if (!ConnectionUtil.canAccess(me.url)) {
                     return GITHUB;
                 }
                 return me;
             }
         }
-        return defaultSource;
-    }
-
-    public static boolean isCN() {
-        return BannerMCStart.I18N.isCN() && ConnectionUtil.measureLatency(CHINA.url.replace("releases/", "")) < ConnectionUtil.measureLatency(MOHIST.url);
+        return MOHIST;
     }
 }
