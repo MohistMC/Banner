741f8081a7fbc04986f3300fdd1764d0eeee1f35
Mgazul <mgazul@163.com>
2024-05-28 01:54:50 +0800

    Fixed #254

--- src/main/java/com/mohistmc/banner/mixin/world/level/MixinClipContext.java
+++ src/main/java/com/mohistmc/banner/mixin/world/level/MixinClipContext.java
@@ -3,18 +3,12 @@ package com.mohistmc.banner.mixin.world.level;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ClipContext.class)
+@Mixin(value = ClipContext.class, priority = 1500)
 public class MixinClipContext {
-
-    @Mutable
-    @Shadow @Final private CollisionContext collisionContext;
 
     @Redirect(method = "<init>", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/world/phys/shapes/CollisionContext;of(Lnet/minecraft/world/entity/Entity;)Lnet/minecraft/world/phys/shapes/CollisionContext;"))
