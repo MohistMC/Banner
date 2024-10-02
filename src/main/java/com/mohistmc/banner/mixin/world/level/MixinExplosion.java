90bc8c517e6275ef0a947bf320eba24eb5e6245b
Mgazul <mgazul@163.com>
2024-10-03 05:22:09 +0800

    Fixed #315

--- src/main/java/com/mohistmc/banner/mixin/world/level/MixinExplosion.java
+++ src/main/java/com/mohistmc/banner/mixin/world/level/MixinExplosion.java
@@ -59,7 +59,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Explosion.class)
+@Mixin(value = Explosion.class, priority = 900)
 public abstract class MixinExplosion implements InjectionExplosion {
 
     // @formatter:off
@@ -227,6 +227,8 @@ public abstract class MixinExplosion implements InjectionExplosion {
 
     }
 
+    public float yield;
+
     /**
      * @author wdog5
      * @reason
@@ -241,6 +243,7 @@ public abstract class MixinExplosion implements InjectionExplosion {
         }
 
         boolean flag = this.interactsWithBlocks();
+
         if (spawnParticles) {
             if (!(this.radius < 2.0F) && flag) {
                 this.level.addParticle(ParticleTypes.EXPLOSION_EMITTER, this.x, this.y, this.z, 1.0D, 0.0D, 0.0D);
@@ -250,53 +253,54 @@ public abstract class MixinExplosion implements InjectionExplosion {
         }
 
         if (flag) {
-            ObjectArrayList<Pair<ItemStack, BlockPos>> objectarraylist = new ObjectArrayList<>();
+            ObjectArrayList<Pair<ItemStack, BlockPos>> objectArrayList = new ObjectArrayList<>();
             boolean flag2 = this.getIndirectSourceEntity() instanceof Player;
             Util.shuffle(this.toBlow, this.level.random);
 
-            // CraftBukkit start
-            org.bukkit.World bworld = this.level.getWorld();
-            org.bukkit.entity.Entity explode = this.source == null ? null : this.source.getBukkitEntity();
-            Location location = new Location(bworld, this.x, this.y, this.z);
+            {
+                // CraftBukkit start
+                org.bukkit.World bworld = this.level.getWorld();
+                org.bukkit.entity.Entity explode = this.source == null ? null : this.source.getBukkitEntity();
+                Location location = new Location(bworld, this.x, this.y, this.z);
 
-            List<org.bukkit.block.Block> blockList = new ObjectArrayList<>();
-            for (int i1 = this.toBlow.size() - 1; i1 >= 0; i1--) {
-                BlockPos cpos = this.toBlow.get(i1);
-                org.bukkit.block.Block bblock = CraftBlock.at(this.level, cpos);
-                if (!bblock.getType().isAir()) {
-                    blockList.add(bblock);
+                List<org.bukkit.block.Block> blockList = new ObjectArrayList<>();
+                for (int i1 = this.toBlow.size() - 1; i1 >= 0; i1--) {
+                    BlockPos cpos = this.toBlow.get(i1);
+                    org.bukkit.block.Block bblock = CraftBlock.at(this.level, cpos);
+                    if (!bblock.getType().isAir()) {
+                        blockList.add(bblock);
+                    }
                 }
-            }
 
-            boolean cancelled;
-            List<org.bukkit.block.Block> bukkitBlocks;
-            float yield;
+                boolean cancelled;
+                List<org.bukkit.block.Block> bukkitBlocks;
 
-            if (explode != null) {
-                EntityExplodeEvent event = new EntityExplodeEvent(explode, location, blockList, this.blockInteraction == Explosion.BlockInteraction.DESTROY_WITH_DECAY ? 1.0F / this.radius : 1.0F);
-                this.level.getCraftServer().getPluginManager().callEvent(event);
-                cancelled = event.isCancelled();
-                bukkitBlocks = event.blockList();
-                yield = event.getYield();
-            } else {
-                BlockExplodeEvent event = new BlockExplodeEvent(location.getBlock(), blockList, this.blockInteraction == Explosion.BlockInteraction.DESTROY_WITH_DECAY ? 1.0F / this.radius : 1.0F);
-                this.level.getCraftServer().getPluginManager().callEvent(event);
-                cancelled = event.isCancelled();
-                bukkitBlocks = event.blockList();
-                yield = event.getYield();
-            }
+                if (explode != null) {
+                    EntityExplodeEvent event = new EntityExplodeEvent(explode, location, blockList, this.blockInteraction == Explosion.BlockInteraction.DESTROY_WITH_DECAY ? 1.0F / this.radius : 1.0F);
+                    this.level.getCraftServer().getPluginManager().callEvent(event);
+                    cancelled = event.isCancelled();
+                    bukkitBlocks = event.blockList();
+                    yield = event.getYield();
+                } else {
+                    BlockExplodeEvent event = new BlockExplodeEvent(location.getBlock(), blockList, this.blockInteraction == Explosion.BlockInteraction.DESTROY_WITH_DECAY ? 1.0F / this.radius : 1.0F);
+                    this.level.getCraftServer().getPluginManager().callEvent(event);
+                    cancelled = event.isCancelled();
+                    bukkitBlocks = event.blockList();
+                    yield = event.getYield();
+                }
 
-            this.toBlow.clear();
-            for (org.bukkit.block.Block bblock : bukkitBlocks) {
-                BlockPos coords = new BlockPos(bblock.getX(), bblock.getY(), bblock.getZ());
-                toBlow.add(coords);
-            }
+                this.toBlow.clear();
+                for (org.bukkit.block.Block bblock : bukkitBlocks) {
+                    BlockPos coords = new BlockPos(bblock.getX(), bblock.getY(), bblock.getZ());
+                    toBlow.add(coords);
+                }
 
-            if (cancelled) {
-                this.wasCanceled = true;
-                return;
+                if (cancelled) {
+                    this.wasCanceled = true;
+                    return;
+                }
+                // CraftBukkit end
             }
-            // CraftBukkit end
 
             ObjectListIterator var5 = this.toBlow.iterator();
 
@@ -327,7 +331,7 @@ public abstract class MixinExplosion implements InjectionExplosion {
 
                         blockstate.spawnAfterBreak(serverLevel, blockpos, ItemStack.EMPTY, flag2);
                         blockstate.getDrops(builder).forEach((itemStack) -> {
-                            addBlockDrops(objectarraylist, itemStack, blockpos1);
+                            addBlockDrops(objectArrayList, itemStack, blockpos1);
                         });
 
                     }
@@ -338,13 +342,19 @@ public abstract class MixinExplosion implements InjectionExplosion {
                 }
             }
 
-            for (Pair<ItemStack, BlockPos> pair : objectarraylist) {
+            var5 = objectArrayList.iterator();
+
+            while(var5.hasNext()) {
+                Pair<ItemStack, BlockPos> pair = (Pair)var5.next();
                 Block.popResource(this.level, pair.getSecond(), pair.getFirst());
             }
         }
 
         if (this.fire) {
-            for (BlockPos blockpos2 : this.toBlow) {
+            ObjectListIterator var13 = this.toBlow.iterator();
+
+            while(var13.hasNext()) {
+                BlockPos blockpos2 = (BlockPos)var13.next();
                 if (this.random.nextInt(3) == 0 && this.level.getBlockState(blockpos2).isAir() && this.level.getBlockState(blockpos2.below()).isSolidRender(this.level, blockpos2.below())) {
                     BlockIgniteEvent event = CraftEventFactory.callBlockIgniteEvent(this.level, blockpos2.getX(), blockpos2.getY(), blockpos2.getZ(), (Explosion) (Object) this);
                     if (!event.isCancelled()) {
