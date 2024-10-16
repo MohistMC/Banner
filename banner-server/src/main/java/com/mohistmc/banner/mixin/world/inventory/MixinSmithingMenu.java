0708d477e1ee453c0a8b235fe4a31b093776cb8a
wdog5 <l1531245281@yandex.com>
2024-10-16 09:13:31 +0800

    Fix PrepareSmithingEvent do not fired

--- banner-server/src/main/java/com/mohistmc/banner/mixin/world/inventory/MixinSmithingMenu.java
+++ banner-server/src/main/java/com/mohistmc/banner/mixin/world/inventory/MixinSmithingMenu.java
@@ -1,16 +1,24 @@
 package com.mohistmc.banner.mixin.world.inventory;
 
+import io.izzel.arclight.mixin.Decorate;
+import io.izzel.arclight.mixin.DecorationOps;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.ItemCombinerMenu;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.inventory.ResultContainer;
 import net.minecraft.world.inventory.SmithingMenu;
+import net.minecraft.world.item.ItemStack;
+import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.inventory.CraftInventory;
 import org.bukkit.craftbukkit.inventory.CraftInventorySmithing;
 import org.bukkit.craftbukkit.inventory.CraftInventoryView;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.event.inventory.PrepareSmithingEvent;
 import org.bukkit.inventory.InventoryView;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(SmithingMenu.class)
 public abstract class MixinSmithingMenu extends ItemCombinerMenu {
@@ -19,6 +27,13 @@ public abstract class MixinSmithingMenu extends ItemCombinerMenu {
 
     public MixinSmithingMenu(@Nullable MenuType<?> menuType, int i, Inventory inventory, ContainerLevelAccess containerLevelAccess) {
         super(menuType, i, inventory, containerLevelAccess);
+    }
+
+    @Decorate(method = "createResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/ResultContainer;setItem(ILnet/minecraft/world/item/ItemStack;)V"))
+    private void banner$prepareSmithing(ResultContainer craftResultInventory, int index, ItemStack stack) throws Throwable {
+        PrepareSmithingEvent event = new PrepareSmithingEvent(getBukkitView(), CraftItemStack.asCraftMirror(stack).clone());
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        DecorationOps.callsite().invoke(craftResultInventory, index, CraftItemStack.asNMSCopy(event.getResult()));
     }
 
     @Override
