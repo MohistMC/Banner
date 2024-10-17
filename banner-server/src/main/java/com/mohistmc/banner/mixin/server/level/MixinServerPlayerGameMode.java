9e061bc88926d30e4a67976945d8ae911c8aeeec
wdog5 <l1531245281@yandex.com>
2024-10-17 15:56:18 +0800

    Revert "Fix MixinServerPlayerGameMode"
    This reverts commit 5d171c4be9a7637b87d4427b4debdbcb59f11d16.

--- banner-server/src/main/java/com/mohistmc/banner/mixin/server/level/MixinServerPlayerGameMode.java
+++ banner-server/src/main/java/com/mohistmc/banner/mixin/server/level/MixinServerPlayerGameMode.java
@@ -1,7 +1,5 @@
 package com.mohistmc.banner.mixin.server.level;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mohistmc.banner.injection.server.level.InjectionServerPlayerGameMode;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -360,8 +358,9 @@ public abstract class MixinServerPlayerGameMode implements InjectionServerPlayer
             at = @At(value = "INVOKE",
             target = "Lnet/minecraft/server/level/ServerLevel;getBlockEntity(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/entity/BlockEntity;",
             shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private void banner$resetState(BlockPos pos, CallbackInfoReturnable<Boolean> cir, @Local LocalRef<BlockState> blockState) {        blockState.set(this.level.getBlockState(pos)); // CraftBukkit - update state from plugins
-        if (blockState.get().isAir()) cir.setReturnValue(false); // CraftBukkit - A plugin set block to air without cancelling
+    private void banner$resetState(BlockPos pos, CallbackInfoReturnable<Boolean> cir, BlockState blockState) {
+        blockState = this.level.getBlockState(pos); // CraftBukkit - update state from plugins
+        if (blockState.isAir()) cir.setReturnValue(false); // CraftBukkit - A plugin set block to air without cancelling
     }
 
     // CraftBukkit start - whole method
